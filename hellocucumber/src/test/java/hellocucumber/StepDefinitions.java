697e1df2554000652c5c17e33a7dbca7b9e12760
NirmalBadodariya <nirmal.inexture@gmail.com>
2022-09-06 16:14:33 +0530

    first one

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -1,39 +1,72 @@
 package hellocucumber;
 
+
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSender;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+
+import java.net.URI;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
-class IsItFriday {
-    static String isItFriday(String today) {
-        return null;
-    }
-}
 
 public class StepDefinitions {
-    private String today;
-    private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
-    }
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
-    }
-//    static String isItFriday(String today) {
-//        return "Nope";
-//    }
+    private Scenario scenario;
+    private Response response;
+    private final String BASE_URL = "https://search-admin-dev-mamb5phriq-uc.a.run.app";
 
-    @Then("I should be told {string}")
-    public String i_should_be_told(String expectedAnswer) {
-        return "Nope";
+
+    @Before
+    public void before(Scenario scenarioVal) {
+        this.scenario = scenarioVal;
     }
-    @When("I ask whether it's Friday yet")
-    public void i_ask_whether_it_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
+
+    @Given("Get Call to {string}")
+    public void get_call_to_url(String url) throws Exception {
+        RestAssured.baseURI = BASE_URL;
+
+        RequestSpecification req = given();
+        req.header("authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkB3ZWJsaW5rdGVjaHMuY29tIiwiZXhwIjoxNjYyNDU1MjU1LCJpYXQiOjE2NjI0NTE2NTV9.EuBihaCMiN6o1627af_GFVvIhiaMjp2oghGxn6qzlKrtxrt3vq7azJ0sY8Ea6Im8LB9a7KJ-f-eNrvitL5NF6A");
+        response = req.when().get(new URI(url));
+    }
+
+    @Then("Response Code {string} is validated")
+    public void response_is_validated(String responseMessage) {
+        int responseCode = response.then().extract().statusCode();
+        Assertions.assertEquals(responseMessage, responseCode+"");
+    }
+
+    private String body = "{\"email\":\"admin@weblinktechs.com\", \"pass\":\"!adg@u^s5FE43q\"}";
+    private String token="";
+    @Before
+    public void setUp(){
+            given().contentType(ContentType.JSON)
+                    .body(body)
+                    .when()
+                    .post("https://search-admin-dev-mamb5phriq-uc.a.run.app/auth/")
+                    .then()
+                    .assertThat()
+                    .statusCode(HttpStatus.SC_OK);
+
+            token = given().contentType(ContentType.JSON)
+                    .body(body)
+                    .when()
+                    .post("https://search-admin-dev-mamb5phriq-uc.a.run.app/authenticate")
+                    .jsonPath()
+                    .get("token");
+
+        System.out.println("toekslkdfsldjfns       : "+token);
     }
 
 
